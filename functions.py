9677c929751789b32fc32dc77adbd3d9295dece4
unknown <hadibaig201@gmail.com>
2026-09-07 23:42:51 -0600

    Reduced Features and used SHAP and XGB IMportances

--- functions.py
+++ functions.py
@@ -5,6 +5,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
 import xgboost
+import shap
+import pandas as pd
+
+
+"""
+    In the new versions of the functions, we have simple removed the inclusion of Winter Wheat, since we  are no longer using it for the latest versions for our Project.
+
+"""
 
 # Making a fucniton we can use reusably when training different Models
 def train_xgb(params, full_df):
@@ -107,6 +115,127 @@ def train_xgb(params, full_df):
 
 
 
+"""
+    We are going to add SHAP values to this function so that we can see what the most influential features were for the model
+"""
+def train_xgb_new(params, full_df, num_features):
+    # Setting up our plotting config
+    fig, axes  = plt.subplots(3,2, figsize=(15, 15))
+    axes = axes.flatten()
+
+
+    # Storing our metrics in the following dicts
+    METRICS = {}
+    accuracies = []
+    train_accuracies = []
+
+    # Storing our Feature Impiortances
+    feature_importances = {}
+
+    # Storing the SHAP values
+    shap_results = {}
+
+    # Getting our unique years
+    years = list(set(full_df['year']))
+    years
+
+    for i, _ in enumerate(years):
+        ax = axes[i]
+        testing_years_dict = {
+            0: 2018,
+            1: 2019,
+            2: 2020,
+            3: 2021,
+            4: 2022
+        }
+        # We are going to select one of the years to validation on
+        # We are going to go from validating on 2018-2022 sequentially
+        year_test = testing_years_dict[i]
+        print(f'Training for Year: {year_test}...')
+
+
+        # We are going to split into train and test
+        # We dont need to standardize our data, since TREE-BASED model dont need standardized data
+        # Filtering by LOYO (Leave One Year Out)
+        training_data = full_df[full_df['year'] != year_test]
+        validation_data = full_df[full_df['year'] == year_test]
+
+        #------TEMPORAL CV-------------------
+        # Now we are going to split the features with the target label.
+        # Our target label is 'class', which has the following values:
+        # {0: 'Barley', 1: 'Oats', 2: 'Winter Wheat', 3: 'Spring Wheat'}
+
+        X_train = training_data.drop(columns=['class', 'year'])
+        y_train = training_data['class']
+
+        X_test = validation_data.drop(columns=['class', 'year'])
+        y_test = validation_data['class']
+
+        # We are going to split into validation set, so we can use it for early stopping
+        X_train, X_val, y_train, y_val = train_test_split(
+            X_train, y_train, train_size=0.85, shuffle=True, random_state=42
+        )
+
+
+        # Instanitaiting our Model
+        xgb_model = xgboost.XGBClassifier(**params, early_stopping_rounds=50, importance_type='gain')
+
+        # Training our XGB Model using the XGBoost API
+        xgb_model.fit(
+            X_train, y_train, 
+            eval_set=[(X_train, y_train), (X_val, y_val)], # We use validation sets here, to avoid data leakage.
+        )
+
+        # Returning only the top specified num of features
+        xgb_importances = pd.DataFrame({
+            'feature': X_train.columns,
+            'importance': xgb_model.feature_importances_
+        })
+        xgb_importances = xgb_importances.sort_values(
+            by='importance',
+            ascending=False
+        )[:num_features]
+        feature_importances[year_test] = xgb_importances
+
+        # Now we are going to add our SHAP code
+        explainer = shap.TreeExplainer(xgb_model)
+        shap_values = explainer(X_test)
+
+        # Appending it to our Shap_results Dict
+        shap_results[year_test] = shap_values
+
+
+        # Evaluating
+        y_pred = xgb_model.predict(X_test)
+        y_pred_train = xgb_model.predict(X_train)
+
+        accuracy_test = accuracy_score(y_test, y_pred)
+        accuracy_train = accuracy_score(y_train, y_pred_train)
+
+        # Appending metrics
+        accuracies.append(accuracy_test)
+        train_accuracies.append(accuracy_train)
+        METRICS[year_test] = accuracy_test
+
+        matrix= confusion_matrix(y_test, y_pred)
+
+        # Visualizing our Confusion Matrix
+        disp = ConfusionMatrixDisplay(confusion_matrix=matrix, 
+                                      display_labels=['Barley', 'Oats', 'Spring_Wheat'])
+        ax.set_title(f'Conf Matrix for {year_test}. Accuracy: {accuracy_test}')
+        disp.plot(ax=ax)
+
+
+    # We have 6 plots. but only using 5, so we hide the unused one
+    axes[5].set_visible(False)
+    plt.tight_layout()
+    plt.show()
+
+    print(f'MEAN ACCURACY: {np.mean(accuracies)}')
+    print(f'MEAN TRAIN ACCURACY: {np.mean(train_accuracies)}')
+    return feature_importances, shap_results
+
+
 # We are going to train our Models with Temporal Cross Validation, or LOYO (Leave One Year Out)
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay, confusion_matrix # We are going to use accuracy and Conf Matrix as our MEtrics
@@ -186,6 +315,92 @@ def train_model(model, full_df):
 
     print(f'MEAN ACCURACY: {np.mean(accuracies)}')
     return METRICS
+
+
+
+"""
+Aslo adding SHAP values
+"""
+
+def train_model_new(model, full_df):
+    # Setting up our plotting config
+    fig, axes  = plt.subplots(3,2, figsize=(15, 15))
+    axes = axes.flatten()
+
+    shap_results = {}
+
+
+    # Storing our metrics in the following dicts
+    METRICS = {}
+    accuracies = []
+
+    # Getting our unique years
+    years = list(set(full_df['year']))
+    years
+
+    for i, _ in enumerate(years):
+        ax = axes[i]
+        testing_years_dict = {
+            0: 2018,
+            1: 2019,
+            2: 2020,
+            3: 2021,
+            4: 2022
+        }
+        # We are going to select one of the years to validation on
+        # We are going to go from validating on 2018-2022 sequentially
+        year_test = testing_years_dict[i]
+        print(f'Training for Year: {year_test}...')
+
+
+        # We are going to split into train and test
+        # We dont need to standardize our data, since TREE-BASED model dont need standardized data
+        # Filtering by LOYO (Leave One Year Out)
+        training_data = full_df[full_df['year'] != year_test]
+        validation_data = full_df[full_df['year'] == year_test]
+
+        #------TEMPORAL CV-------------------
+        # Now we are going to split the features with the target label.
+        # Our target label is 'class', which has the following values:
+        # {0: 'Barley', 1: 'Oats', 2: 'Winter Wheat', 3: 'Spring Wheat'}
+
+        X_train = training_data.drop(columns=['class', 'year'])
+        y_train = training_data['class']
+
+        # Trainging Our Model
+        model.fit(X_train, y_train)
+
+        # Adding our SHAP code
+        explainer = shap.TreeExplainer(model)
+        shap_values = explainer(X_test)
+        shap_results[year_test] = shap_values
+
+        # Getting our Validation results
+        X_test = validation_data.drop(columns=['class', 'year'])
+        y_test = validation_data['class']
+
+        y_pred = model.predict(X_test)
+
+        #-------METRICS-----------
+        accuracy = accuracy_score(y_test, y_pred)
+
+        # appending our Accuracy to our MEtrics dictionary
+        METRICS[year_test] = accuracy
+        accuracies.append(accuracy)
+
+        matrix = confusion_matrix(y_test, y_pred)
+        disp  =ConfusionMatrixDisplay(confusion_matrix=matrix, 
+                                    display_labels=['Barley', 'Oats', 'Spring_Wheat'])
+        ax.set_title(f'Confusion Matrix for: {year_test}. Accuracy: {accuracy}')
+        disp.plot(ax=ax)
+
+    # We have 6 plots. but only using 5, so we hide the unused one
+    axes[5].set_visible(False)
+    plt.tight_layout()
+    plt.show()
+
+    print(f'MEAN ACCURACY: {np.mean(accuracies)}')
+    return METRICS, shap_results
 
 
 
